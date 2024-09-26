845c7400e6714b5b77a1b0d528fa7895d40bd963
citronoiseau <avroraka2004@gmail.com>
2024-09-26 13:43:34 +0300

    added functions to move paddles

--- frontend/src/classes/paddle.js
+++ frontend/src/classes/paddle.js
@@ -1,13 +1,35 @@
 export default class Paddle {
-  constructor(x, y, width, height) {
+  constructor(x, y, type, width, height) {
     this.x = x;
     this.y = y;
+    this.type = type;
+    this.speed = 5;
+    this.direction = 0;
     this.width = width;
     this.height = height;
   }
 
-  movePaddle(dx) {
-    this.x += dx;
+  checkCollision(canvas) {
+    if (this.y < 0) {
+      this.y = 0;
+    } else if (this.y + this.height > canvas.height) {
+      this.y = canvas.height - this.height;
+    }
+  }
+
+  movePaddle(canvas) {
+    this.y += this.speed * this.direction;
+    this.checkCollision(canvas);
+  }
+
+  reset(canvas) {
+    if (this.type === "left") {
+      this.x = 40;
+      this.y = canvas.height / 2 - 50;
+    } else {
+      this.x = canvas.width - 50;
+      this.y = canvas.height / 2 - 50;
+    }
   }
 
   draw(ctx) {
--- frontend/src/modules/controller.js
+++ frontend/src/modules/controller.js
@@ -6,8 +6,8 @@ import gameUI from "../DOM/gameUI";
 
 let isPaused = true;
 
-function updateScore(board, player1Score, player2Score) {
-  const scoreCanvas = document.querySelector(".scoreCanvas");
+function updateScore(board) {
+  const scoreCanvas = document.querySelector(".scoreCanvas"); // For ui element that don't change often
   const scoreCtx = scoreCanvas.getContext("2d");
 
   scoreCtx.clearRect(0, 0, scoreCtx.canvas.width, scoreCtx.canvas.height);
@@ -16,7 +16,7 @@ function updateScore(board, player1Score, player2Score) {
   scoreCtx.textAlign = "center";
 
   scoreCtx.fillText(
-    `Player 1: ${player1Score}`,
+    `${board.players[0].name}: ${board.players[0].score}`,
     scoreCtx.canvas.width / 4,
     scoreCtx.canvas.height / 2,
   );
@@ -26,10 +26,33 @@ function updateScore(board, player1Score, player2Score) {
     scoreCtx.canvas.height / 2,
   );
   scoreCtx.fillText(
-    `Player 2: ${player2Score}`,
+    `${board.players[1].name}: ${board.players[1].score}`,
     (3 * scoreCtx.canvas.width) / 4,
     scoreCtx.canvas.height / 2,
   );
+}
+
+function displayWinner(winner) {
+  const scoreCanvas = document.querySelector(".scoreCanvas");
+  const scoreCtx = scoreCanvas.getContext("2d");
+
+  scoreCtx.clearRect(0, 0, scoreCtx.canvas.width, scoreCtx.canvas.height);
+
+  scoreCtx.font = "30px Arial";
+  scoreCtx.textAlign = "center";
+  scoreCtx.textBaseline = "middle";
+
+  const centerX = scoreCtx.canvas.width / 2;
+  const centerY = scoreCtx.canvas.height / 2;
+  if (winner === "It's a tie!") {
+    scoreCtx.fillText(`${winner}`, centerX, centerY);
+  } else {
+    scoreCtx.fillText(
+      `${winner.name} won with score ${winner.score}`,
+      centerX,
+      centerY,
+    );
+  }
 }
 
 function gameLoop(board) {
@@ -41,6 +64,13 @@ function gameLoop(board) {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  player1.paddle.movePaddle(canvas);
+  if (player2.type === "bot") {
+    // todo
+  } else {
+    player2.paddle.movePaddle(canvas);
+  }
+
   player1.paddle.draw(ctx);
   player2.paddle.draw(ctx);
 
@@ -49,33 +79,52 @@ function gameLoop(board) {
     const isWin = ball.moveBall(canvas, player1, player2);
     if (isWin) {
       isPaused = true;
-      updateScore(board, player1.score, player2.score);
+      updateScore(board);
+      if (board.rounds === 5) {
+        let winner;
+        if (player1.score > player2.score) {
+          winner = player1;
+        } else if (player1.score < player2.score) {
+          winner = player2;
+        } else {
+          winner = "It's a tie!";
+        }
+
+        displayWinner(winner);
+        return;
+      }
+
       startCountdown(board);
       ball.reset(canvas);
+      player1.paddle.reset(canvas);
+      player2.paddle.reset(canvas);
       ball.setSpeed(Math.random() < 0.5 ? ball.dx : -ball.dx);
     }
     requestAnimationFrame(() => gameLoop(board));
   }
 }
 
-export default function controller() {
+export default function controller(bot = true) {
+  let gameStarted = false;
   gameUI();
   const canvas = document.querySelector(".canvas");
   const ctx = canvas.getContext("2d"); // Gives me canvas workspace
 
-  const scoreCanvas = document.querySelector(".scoreCanvas"); // For ui element that don't change often
-  const scoreCtx = scoreCanvas.getContext("2d");
-
-  const player1Paddle = new Paddle(40, canvas.height / 2 - 50, 10, 100);
+  const player1Paddle = new Paddle(40, canvas.height / 2 - 50, "left", 10, 100);
   const player1 = new Player("player", "Player 1", player1Paddle, 0);
 
   const player2Paddle = new Paddle(
     canvas.width - 50,
     canvas.height / 2 - 50,
+    "right",
     10,
     100,
   );
-  const player2 = new Player("player", "Player 2", player2Paddle, 0);
+
+  const player2 = bot
+    ? new Player("bot", "Bot", player2Paddle, 0)
+    : new Player("player", "Player 2", player2Paddle, 0);
+
   const players = [player1, player2];
 
   const ball = new Ball(2, 2, canvas.width / 2, canvas.height / 2, 10);
@@ -84,7 +133,7 @@ export default function controller() {
 
   player1.paddle.draw(ctx);
   player2.paddle.draw(ctx);
-  updateScore(board, player1.score, player2.score);
+  updateScore(board);
 
   ctx.font = "48px serif";
   ctx.textAlign = "center";
@@ -94,10 +143,37 @@ export default function controller() {
   const x = canvas.width / 2;
   const y = canvas.height / 2;
   ctx.fillText(text, x, y);
-
+  paddleController(player1, player2);
   document.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !gameStarted) {
+      gameStarted = true;
       startCountdown(board);
+    }
+  });
+}
+
+function paddleController(player1, player2) {
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "w") {
+      player1.paddle.direction = -1;
+    } else if (e.key === "s") {
+      player1.paddle.direction = 1;
+    }
+
+    if (e.key === "ArrowUp") {
+      player2.paddle.direction = -1;
+    } else if (e.key === "ArrowDown") {
+      player2.paddle.direction = 1;
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "w" || e.key === "s") {
+      player1.paddle.direction = 0;
+    }
+
+    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+      player2.paddle.direction = 0;
     }
   });
 }
