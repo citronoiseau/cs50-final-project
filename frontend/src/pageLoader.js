44b5f20f96523d79483167713d32d32508b64368
citronoiseau <avroraka2004@gmail.com>
2024-10-01 17:15:38 +0300

    added more handling with deep link

--- frontend/src/pageLoader.js
+++ frontend/src/pageLoader.js
@@ -1,10 +1,27 @@
 import startMenu from "./DOM/startMenu";
 import controllerMultiplayer from "./modules/multiplayer";
+import showToast from "./DOM/helpers/showToast";
 
 export function pageLoader() {
   startMenu();
 }
 
-export function pageLoaderDeepLink(gameId) {
-  controllerMultiplayer(gameId);
+export async function pageLoaderDeepLink(gameId) {
+  try {
+    const response = await controllerMultiplayer(gameId);
+    if (!response.ok) {
+      showToast(
+        "Failed to join the game. Please check the game ID and try again.",
+        true,
+      );
+      const newUrl = window.location.origin + window.location.pathname;
+      window.history.replaceState({}, document.title, newUrl);
+      pageLoader();
+    }
+  } catch (error) {
+    const newUrl = window.location.origin + window.location.pathname;
+    window.history.replaceState({}, document.title, newUrl);
+    showToast("An error occurred", true);
+    pageLoader();
+  }
 }
